5fad0894c7fb3dc2656397bd829669623a3f9357
AleksandrLiadov <72351793+AleksandrLiadov@users.noreply.github.com>
2025-01-06 16:43:31 +0100

    (fix) Fix one line comment (#7)
    Co-authored-by: Aleksandr Liadov <aleksandr.liadov@stellantis.com>

--- core/schemas/comment_chains.py
+++ core/schemas/comment_chains.py
@@ -1,13 +1,21 @@
-from pydantic import BaseModel, computed_field
+from pydantic import BaseModel, computed_field, model_validator
+from typing_extensions import Self
 
 from core.tokenizer import get_token_count
 
 
 class CommentChain(BaseModel):
-    start_line: int
+    start_line: int | None
     end_line: int
     top_level_comment_id: int
     comment: str = ""
+
+    @model_validator(mode='after')
+    def check_one_line_comment(self) -> Self:
+        # If the comment is only one line, set the start_line to the end_line
+        if self.start_line is None:
+            self.start_line = self.end_line
+        return self
 
     def __str__(self) -> str:
         lines = "\n".join([f"  {line}" for line in self.comment.split("\n")])
